1a4cb24408d3fb994be5e12fcd974aeb9a0e2a51
harsharora99 <42893467+harsharora99@users.noreply.github.com>
2020-10-21 20:14:02 +0530

    Create tarjan.cpp

---
+++ tarjan.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//Here comes the code to find SCCs(Strongly Connected Components) in directed graphs using Tarjan's algo.
+
+int main(){
+
+  return 0;
+}
